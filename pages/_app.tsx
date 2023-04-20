0b33eb815bebfb29f9d3237c1dcba285af54934e
Jorge Venegas <jorgepvenegas@protonmail.com>
2023-04-20 09:58:16 -0700

    Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import Link from "next/link";
 import { PrismicProvider } from "@prismicio/react";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "../prismicio";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
       <PrismicPreview repositoryName={repositoryName}>
         <Component {...pageProps} />
+        <Analytics />
       </PrismicPreview>
     </PrismicProvider>
   );
