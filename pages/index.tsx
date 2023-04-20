68a354faf01a76fee95fa2b345f26700a2387b4e
Jorge Venegas <jorgepvenegas@protonmail.com>
2023-04-20 12:16:15 -0700

    Responsive bla bla bla

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import type { InferGetStaticPropsType, GetStaticPropsContext } from "next";
 import { createClient } from "../prismicio";
 
@@ -19,57 +18,53 @@ export async function getStaticProps({ previewData }: GetStaticPropsContext) {
 
 export default function Home({ page }: PageProps) {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Jorge Venegas Photography</title>
         <meta name="description" content="A space for my photography hobby" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className="text-3xl">
-          Photos by <a href="https://jorgepvenegas.com">Jorge</a>
-        </h1>
-
-        <div className={styles.grid}>
-          <div className="container mx-auto px-5 py-2">
-            <div className="flex flex-wrap md:-m-2">
-              {page.results.map((p, i) => (
-                <div key={i} className="flex w-1/4 flex-wrap">
-                  <div className="w-full p-1 md:p-2">
-                    <Image
-                      src={p.data.image.url}
-                      alt={p.data.image.alt}
-                      width={720}
-                      height={480}
-                      sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw"
-                      style={{
-                        objectFit: "cover",
-                        transform: "translate3d(0, 0, 0)",
-                      }}
-                      className="block h-full w-full rounded-lg object-cover object-center brightness-100 transition hover:brightness-110"
-                    />
+      <main className="min-h-screen flex items-center">
+        <div className="mx-auto max-w-screen-2xl p-5 xl:px-10">
+          <div className="grid gap-3 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+            {page.results.map((p, i) => {
+              return (
+                <div key={p.id} className="relative">
+                  <Image
+                    src={p.data.image.url}
+                    alt={p.data.image.alt}
+                    width={720}
+                    height={480}
+                    sizes="(max-width: 640px) 100vw,
+                    (max-width: 1280px) 50vw,
+                    (max-width: 1536px) 33vw,
+                    25vw"
+                    style={{
+                      objectFit: "cover",
+                      transform: "translate3d(0, 0, 0)",
+                    }}
+                    className="rounded-lg object-cover object-center brightness-100 transition hover:brightness-110 grow h-full"
+                  />
+                  <div className="absolute bottom-0 left-0 w-full">
+                    <h3 className=" backdrop-blur bg-white/20 p-3 rounded-b-lg text-white font-normal font-mono text-sm">
+                      {p.data.image.alt || "None"}
+                    </h3>
                   </div>
                 </div>
-              ))}
-            </div>
+              );
+            })}
           </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="flex flex-1 py-5 border-t border-gray-200 items-center justify-center">
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://jorgepvenegas.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Jorge Venegas{" "}
         </a>
       </footer>
     </div>
