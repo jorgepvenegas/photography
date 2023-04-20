3fdf739b4d1d98449ef7e7aebe89290ef00025c3
Jorge Venegas <jorgepvenegas@protonmail.com>
2023-04-19 18:37:44 -0700

    Pretty cool stuff

---
+++ .slicemachine/assets/slices/Header/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Header';
+
+export default {
+  title: 'slices/Header'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Thing","spans":[]}],"description":[{"type":"paragraph","text":"Commodo voluptate occaecat eu eiusmod anim cupidatat in enim incididunt nulla adipisicing consequat ex. Consequat nisi aliqua exercitation Lorem. Lorem elit incididunt Lorem non laborum ex nisi in duis.","spans":[]}]},"id":"_Default","slice_type":"header"}} />
+_Default.storyName = ''
