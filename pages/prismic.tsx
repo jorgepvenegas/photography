00d65a629d33a2ca7f69c39086c476c5f3e3d1df
Jorge Venegas <jorgepvenegas@protonmail.com>
2023-04-20 12:29:40 -0700

    with Prismic

--- pages/prismic.tsx
+++ pages/prismic.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import type { InferGetStaticPropsType, GetStaticPropsContext } from "next";
 import { createClient } from "../prismicio";
-import Link from "next/link";
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
@@ -17,7 +16,7 @@ export async function getStaticProps({ previewData }: GetStaticPropsContext) {
   };
 }
 
-export default function Home({ page }: PageProps) {
+export default function PrismicPage({ page }: PageProps) {
   return (
     <div>
       <Head>
@@ -28,14 +27,34 @@ export default function Home({ page }: PageProps) {
 
       <main className="min-h-screen flex items-center">
         <div className="mx-auto max-w-screen-2xl p-5 xl:px-10">
+          <h1 className="text-4xl pb-10 font-thin">Example with Prismic</h1>
           <div className="grid gap-3 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-            <div className="">
-              <Link href="/prismic">
-                <h3 className="p-3 rounded-sm text-slate-800 font-normal font-mono text-sm hover:bg-slate-200">
-                  Prismic
-                </h3>
-              </Link>
-            </div>
+            {page.results.map((p, i) => {
+              return (
+                <div key={p.id} className="relative">
+                  <Image
+                    src={p.data.image.url}
+                    alt={p.data.image.alt}
+                    width={720}
+                    height={480}
+                    sizes="(max-width: 640px) 100vw,
+                    (max-width: 1280px) 50vw,
+                    (max-width: 1536px) 33vw,
+                    25vw"
+                    style={{
+                      objectFit: "cover",
+                      transform: "translate3d(0, 0, 0)",
+                    }}
+                    className="rounded-lg object-cover object-center brightness-100 transition hover:brightness-110 grow h-full"
+                  />
+                  <div className="absolute bottom-0 left-0 w-full">
+                    <h3 className=" backdrop-blur bg-white/20 p-3 rounded-b-lg text-white font-normal font-mono text-sm">
+                      {p.data.image.alt || "None"}
+                    </h3>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </main>
